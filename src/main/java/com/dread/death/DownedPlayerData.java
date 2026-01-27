4fac2950ac044a7ad11ef275ecb6d97730b72e9d
dylan <dylan@localhost>
2026-01-26 20:41:52 -0500

    feat(11-01): extend DownedPlayerData with mode field and NBT persistence
    - Add mode field (DreadGameMode type)
    - Update constructor to accept DreadGameMode parameter
    - Add NBT serialization for mode (with backwards compatibility)
    - Default to MULTIPLAYER if mode missing or invalid in NBT

--- src/main/java/com/dread/death/DownedPlayerData.java
+++ src/main/java/com/dread/death/DownedPlayerData.java
@@ -1,5 +1,6 @@
 package com.dread.death;
 
+import com.dread.death.GameModeDetector.DreadGameMode;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 import java.util.UUID;
@@ -11,17 +12,27 @@ public class DownedPlayerData {
     public final UUID playerId;
     public BlockPos downedPos;
     public int remainingTicks;  // 300 seconds = 6000 ticks
+    public DreadGameMode mode;
 
-    public DownedPlayerData(UUID playerId, BlockPos downedPos, int remainingTicks) {
+    public DownedPlayerData(UUID playerId, BlockPos downedPos, int remainingTicks, DreadGameMode mode) {
         this.playerId = playerId;
         this.downedPos = downedPos;
         this.remainingTicks = remainingTicks;
+        this.mode = mode;
     }
 
     public DownedPlayerData(NbtCompound nbt) {
         this.playerId = nbt.getUuid("PlayerId");
         this.downedPos = BlockPos.fromLong(nbt.getLong("DownedPos"));
         this.remainingTicks = nbt.getInt("RemainingTicks");
+
+        // Read mode from NBT, default to MULTIPLAYER for backwards compatibility
+        try {
+            String modeStr = nbt.getString("Mode");
+            this.mode = modeStr.isEmpty() ? DreadGameMode.MULTIPLAYER : DreadGameMode.valueOf(modeStr);
+        } catch (IllegalArgumentException e) {
+            this.mode = DreadGameMode.MULTIPLAYER;
+        }
     }
 
     public NbtCompound toNbt() {
@@ -29,6 +40,7 @@ public class DownedPlayerData {
         nbt.putUuid("PlayerId", playerId);
         nbt.putLong("DownedPos", downedPos.asLong());
         nbt.putInt("RemainingTicks", remainingTicks);
+        nbt.putString("Mode", mode.name());
         return nbt;
     }
 
